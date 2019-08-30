3f7a1cb62ee6e38af36e7e80a25bb28dc783c7a9
jigracia <jigracia@miuandes.cl>
2019-08-30 10:23:24 -0400

    Metodo ReadCard implementado

--- Laboratorio_3_OOP_201902/Game.cs
+++ Laboratorio_3_OOP_201902/Game.cs
@@ -1,6 +1,7 @@
 ﻿using Laboratorio_3_OOP_201902.Cards;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Laboratorio_3_OOP_201902
@@ -17,8 +18,9 @@ namespace Laboratorio_3_OOP_201902
         //Constructor
         public Game()
         {
-
+            this.decks = new List<Deck>();
         }
+        
 
         //Propiedades
         public Player[] Players
@@ -91,6 +93,41 @@ namespace Laboratorio_3_OOP_201902
         public void Play()
         {
             throw new NotImplementedException();
+        }
+
+        public void ReadCards() {
+            int cont = 0;
+            string fileLocation = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + @"\Files\Decks.txt";
+            StreamReader reader = new StreamReader(fileLocation);
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] position = line.Split();
+                List<Card> cards = new List<Card>();
+                if (position[0]!="END")
+                {
+                    if (position[0]!="START")
+                    {
+                        switch (position[0])
+                        {
+                            case "CombatCard":
+                                
+                                cards.Add(new CombatCard(position[1], position[2], position[3], int.Parse(position[4]), bool.Parse(position[5])));
+                                break;
+                            case "SpecialCard":
+                                cards.Add(new SpecialCard(position[1], position[2],position[3]));
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    cont++;
+                    Decks[cont].Cards = new List<Card>(cards);
+                }
+                
+            }
+            reader.Close();
         }
     }
 }
